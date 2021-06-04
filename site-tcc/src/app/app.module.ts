007fd61ca1e65b2828974d84988364e93ffcf588
Debora C. Fernandes <dehcarrascoza@gmail.com>
2021-06-04 15:13:01 -0300

    separacao em componentes

--- site-tcc/src/app/app.module.ts
+++ site-tcc/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { IntroducaoComponent } from './introducao/introducao.component';
+import { VoceSabiaComponent } from './voce-sabia/voce-sabia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    RodapeComponent,
+    IntroducaoComponent,
+    VoceSabiaComponent
   ],
   imports: [
     BrowserModule,
---
+++ site-tcc/src/app/voce-sabia/voce-sabia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-voce-sabia',
+  templateUrl: './voce-sabia.component.html',
+  styleUrls: ['./voce-sabia.component.css']
+})
+export class VoceSabiaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
